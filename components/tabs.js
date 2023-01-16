cb2057f7c56d629af31c75655c5c68c82f1a6de9
Jonathan Almeida <2251555+slipjfa@users.noreply.github.com>
2023-01-16 12:59:05 -0300

    prettier

--- components/tabs.js
+++ components/tabs.js
@@ -3,30 +3,29 @@ import React, { useState } from "react";
 const Tabs = (playersCount) => {
   const sorted = playersCount.players;
   const [activeTab, setActiveTab] = useState(0);
-  const allTabs = ["All", "+2", "+8", "+14", "+20"]; 
-  
+  const allTabs = ["All", "+2", "+8", "+14", "+20"];
+
   function DisplayTab(activeTab) {
-    if (activeTab === 0){
+    if (activeTab === 0) {
       return <FirstTab total={sorted.total} />;
     } else {
-      return <Tab activeTabInfo={Object.values(sorted)[activeTab]} />
+      return <Tab activeTabInfo={Object.values(sorted)[activeTab]} />;
     }
   }
   return (
     <div className="Tabs">
       <ul className="nav">
-        {
-          allTabs.map((tab, tabNumber) => {
-            return (
-              <li key={tabNumber}
-                className={activeTab === (tabNumber) ? "active" : ""}
-                onClick={() => setActiveTab(tabNumber)}
-              >
-                {tab} ({Object.values(sorted)[tabNumber].length})
-              </li>
-            )
-          })
-        }
+        {allTabs.map((tab, tabNumber) => {
+          return (
+            <li
+              key={tabNumber}
+              className={activeTab === tabNumber ? "active" : ""}
+              onClick={() => setActiveTab(tabNumber)}
+            >
+              {tab} ({Object.values(sorted)[tabNumber].length})
+            </li>
+          );
+        })}
       </ul>
       <div className="outlet">{DisplayTab(activeTab)}</div>
     </div>
@@ -34,16 +33,18 @@ const Tabs = (playersCount) => {
 };
 
 const Tab = (activeTabInfo) => {
-  return <div className={"Tab"} >{DisplayNames(Object.values(activeTabInfo)[0])}</div>
+  return (
+    <div className={"Tab"}>{DisplayNames(Object.values(activeTabInfo)[0])}</div>
+  );
 };
 
 const FirstTab = (total) => {
-  return <div className="FirstTab" >{DisplayAllNamesAndTime(total)}</div>;
+  return <div className="FirstTab">{DisplayAllNamesAndTime(total)}</div>;
 };
 
 const DisplayNames = (timeSlot) => {
   return timeSlot.map((name, index) => {
-    return [
+    return (
       <div key={index}>
         <input
           type="text"
@@ -55,15 +56,16 @@ const DisplayNames = (timeSlot) => {
         />
         <br />
       </div>
-    ]
-  })
-}
+    );
+  });
+};
 
 const DisplayAllNamesAndTime = (players) => {
   return players.total.map((player) => {
     return (
       <div key={player._id}>
-        <input className="input-name"
+        <input
+          className="input-name"
           type="text"
           name="playername"
           id="playername"
@@ -71,25 +73,23 @@ const DisplayAllNamesAndTime = (players) => {
           value={player.name}
         />
         <br />
-        {
-          player.timeSlots.map((slot, index) => {
-            return (
-              <label className="switch"  key={index}>
-                <input
-                  type="checkbox"
-                  name={"time-slot"+index}
-                  id={"time-slot"+index}
-                  checked={(slot) ? "checked" : ""}
-                  readOnly
-                />
-                <span className="slider"></span>
-              </label>
-            ) 
-          })
-        }
+        {player.timeSlots.map((slot, index) => {
+          return (
+            <label className="switch" key={index}>
+              <input
+                type="checkbox"
+                name={"time-slot" + index}
+                id={"time-slot" + index}
+                checked={slot ? "checked" : ""}
+                readOnly
+              />
+              <span className="slider"></span>
+            </label>
+          );
+        })}
       </div>
     );
   });
-} 
+};
 
-export default Tabs
+export default Tabs;
